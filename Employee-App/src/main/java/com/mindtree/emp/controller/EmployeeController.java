b5ae8413c0f2ab13bffa9b32748d3693757581a1
vimaleshn98 <vimaleshn98@gmail.com>
2021-02-15 17:08:37 +0530

    updated

--- Employee-App/src/main/java/com/mindtree/emp/controller/EmployeeController.java
+++ Employee-App/src/main/java/com/mindtree/emp/controller/EmployeeController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mindtree.emp.entity.Employee;
-import com.mindtree.emp.exception.daoexception.EmployeeNotFoundDaoException;
 import com.mindtree.emp.exception.serviceException.EmployeeInputServiceException;
 import com.mindtree.emp.exception.serviceException.EmployeeNotFoundServiceException;
 import com.mindtree.emp.exception.serviceException.IntenalServerServiceException;
--- Employee-App/src/main/java/com/mindtree/emp/service/EmployeeService.java
+++ Employee-App/src/main/java/com/mindtree/emp/service/EmployeeService.java
@@ -1,36 +1,38 @@
 package com.mindtree.emp.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import com.mindtree.emp.Dao.EmployeeDao;
 import com.mindtree.emp.entity.Employee;
-import com.mindtree.emp.exception.daoexception.EmployeeInputDaoException;
-import com.mindtree.emp.exception.daoexception.EmployeeNotFoundDaoException;
-import com.mindtree.emp.exception.daoexception.IntenalServerDaoException;
 import com.mindtree.emp.exception.serviceException.EmployeeInputServiceException;
 import com.mindtree.emp.exception.serviceException.EmployeeNotFoundServiceException;
 import com.mindtree.emp.exception.serviceException.IntenalServerServiceException;
+import com.mindtree.emp.util.EmployeeRepository;
 
 @Service
 public class EmployeeService {
 	@Autowired
-	EmployeeDao emp;
+	EmployeeRepository emp;
 
 	public ResponseEntity<List<Employee>> getEmployees()
 			throws EmployeeInputServiceException, IntenalServerServiceException {
 		// TODO Auto-generated method stub
+		System.out.println(emp.findAll().get(1));
 		try {
-			return emp.save();
-		} catch (EmployeeInputDaoException e) {
-			// TODO Auto-generated catch block
-			throw new EmployeeInputServiceException(e.getLocalizedMessage());
-		} catch (IntenalServerDaoException e) {
-			// TODO Auto-generated catch block
-			throw new IntenalServerServiceException(e.getLocalizedMessage());
+			if (emp.findAll().get(1)!=null) {
+				return new ResponseEntity<List<Employee>>(emp.findAll(), HttpStatus.OK);
+			} else {
+				throw new EmployeeInputServiceException("No Employess");
+			}
+		} catch (RuntimeException e) {
+			// TODO: handle exception
+			throw new IntenalServerServiceException("Server error");
 		}
 	}
 
@@ -38,51 +40,45 @@ public class EmployeeService {
 			throws IntenalServerServiceException, EmployeeInputServiceException {
 		// TODO Auto-generated method stub
 		try {
-			return emp.add(empadd);
-		} catch (EmployeeInputDaoException e) {
-			// TODO Auto-generated catch block
-			throw new EmployeeInputServiceException(e.getLocalizedMessage());
-		} catch (IntenalServerDaoException e) {
-			// TODO Auto-generated catch block
-			throw new IntenalServerServiceException(e.getLocalizedMessage());
+			if (emp.save(empadd).getEmpName() != null) {
+				return new ResponseEntity<>("Employee is Added", HttpStatus.CREATED);
+			} else {
+				throw new EmployeeInputServiceException("Insertion Error ");
+			}
+		} catch (RuntimeException e) {
+			// TODO: handle exception
+			throw new IntenalServerServiceException("Server error");
 		}
 	}
 
 	public ResponseEntity<Employee> getEmployeeById(int id)
 			throws EmployeeNotFoundServiceException, EmployeeInputServiceException, IntenalServerServiceException {
-		// TODO Auto-generated method stub
 		try {
-			return emp.get(id);
-		} catch (EmployeeInputDaoException e) {
-			// TODO Auto-generated catch block
-			throw new EmployeeInputServiceException(e.getLocalizedMessage());
-		} catch (IntenalServerDaoException e) {
-			// TODO Auto-generated catch block
-			throw new IntenalServerServiceException(e);
-		} catch (EmployeeNotFoundDaoException e) {
-			// TODO Auto-generated catch block
-			throw new EmployeeNotFoundServiceException(e);
-		}
-
+			return new ResponseEntity<>(emp.findById(id).get(), HttpStatus.OK);
+			}
+			catch (MethodArgumentTypeMismatchException | NumberFormatException e) {
+				throw new EmployeeInputServiceException("Please enter integer value ");
+			}
+			catch (NoSuchElementException e) {
+				throw new EmployeeNotFoundServiceException("Employee not found");
+			}
+			catch (RuntimeException e) {
+				throw new IntenalServerServiceException("Server Exception");
+			}
 	}
 
 	public ResponseEntity<String> deleteById(int id) throws EmployeeNotFoundServiceException {
 		// TODO Auto-generated method stub
-		try {
-			return emp.delete(id);
-		} catch (EmployeeNotFoundDaoException e) {
-			// TODO Auto-generated catch block
-			throw new EmployeeNotFoundServiceException(e);
-		}
+		Employee deleteEmp =emp.findById(id).orElseThrow( ()->new EmployeeNotFoundServiceException("Employee not found"));
+		emp.delete(deleteEmp);
+		return new ResponseEntity<>("Employee deleted successfully", HttpStatus.NO_CONTENT);
 	}
 
 	public ResponseEntity<String> updateSalary(float upsalary, int id) throws EmployeeNotFoundServiceException {
 		// TODO Auto-generated method stub
-		try {
-			return emp.update(upsalary, id);
-		} catch (EmployeeNotFoundDaoException e) {
-			// TODO Auto-generated catch block
-			throw new EmployeeNotFoundServiceException(e);
-		}
+		Employee updatedEmp = emp.findById(id).orElseThrow( ()->new EmployeeNotFoundServiceException("Employee not found"));
+		updatedEmp.setEmpSalary(upsalary);
+		emp.save(updatedEmp);
+		return new ResponseEntity<>("Salary updated successfully", HttpStatus.OK);
 	}
 }
